42679212387e9c55463822bb52f543273a2b1a04
CoditaV22 <133143695+CoditaV22@users.noreply.github.com>
2023-09-01 11:43:17 +0300

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -3,24 +3,35 @@
 #include <stdio.h>
 #include "uart.h"
 #include "rtc.h"
+#include "systick.h"
+#include "gpio.h"
 
 #define BUFF_LEN	14
+
 
 static void display_calendar(void);
 
 uint8_t time_buffer[BUFF_LEN] = {0};
 uint8_t date_buffer[BUFF_LEN] = {0};
+
+char key;
+
+
+static void uart_callback(void);
 int main(void)
 {
 
 
 
 	uart2_rxtx_init();
+	uart2_rx_interrupt_init();
 	rtc_init();
 
 	while(1)
 	{
 		display_calendar();
+		GPIO_set_light();
+		systickDelayMs(500);
 
 	}
 }
@@ -38,9 +49,30 @@ static void display_calendar(void)
 	sprintf((char *)date_buffer , "%.2d - %.2d - %.2d" ,rtc_convert_bcd2bin(rtc_date_get_day())
 													   ,rtc_convert_bcd2bin(rtc_date_get_month())
 													   ,rtc_convert_bcd2bin(rtc_date_get_year()));
-
-	printf("Date : %.2d - %.2d - %.2d\n\r" 			   ,rtc_convert_bcd2bin(rtc_date_get_day())
-													   ,rtc_convert_bcd2bin(rtc_date_get_month())
-													   ,rtc_convert_bcd2bin(rtc_date_get_year()));
 }
+
+static void uart_callback(void)
+{
+	key =  USART2->RDR;
+
+	if(key == '1')
+		{
+		printf("Date : %.2d - %.2d - %.2d\n\r" 			   ,rtc_convert_bcd2bin(rtc_date_get_day())
+															   ,rtc_convert_bcd2bin(rtc_date_get_month())
+															   ,rtc_convert_bcd2bin(rtc_date_get_year()));
+		}
+}
+
+void USART2_IRQHandler(void)
+{
+	/*ChecK if RXNE is set*/
+
+	if(USART2-> ISR & ISR_RXNE)
+	{
+		//Do something
+		uart_callback();
+
+	}
+}
+
 
