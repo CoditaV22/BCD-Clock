4534fc1f7a606b1a8d5219e01988ea3f9c666ca5
CoditaV22 <133143695+CoditaV22@users.noreply.github.com>
2023-09-09 14:49:16 +0300

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -1,29 +1,30 @@
 #include "stm32f0xx.h"
 #include <stdint.h>
+#include <string.h>
 #include <stdio.h>
 #include "uart.h"
 #include "rtc.h"
 #include "systick.h"
 #include "gpio.h"
 
-#define BUFF_LEN	14
 
-
-static void display_calendar(void);
+static void get_calendar(void);
 static void SysTick_callback(void);
 static void uart_callback(void);
 
-uint8_t time_buffer[BUFF_LEN] = {0};
-uint8_t date_buffer[BUFF_LEN] = {0};
+uint8_t time_buffer[12] = {0};
+uint8_t date_buffer[12] = {0};
+uint8_t alr_set;
 
 char key;
+char alarm_time[12] = "13 : 26 : 10";
 
 int main(void)
 {
-
 	uart2_rx_interrupt_init();
 	rtc_init();
 	systick_1Hz_interrupt();
+	alr_set = 0;
 
 	while(1){
 
@@ -31,17 +32,11 @@ int main(void)
 
 }
 
-static void display_calendar(void)
+static void get_calendar(void)
 {
 	sprintf((char *)time_buffer , "%.2d : %.2d : %.2d" ,rtc_convert_bcd2bin(rtc_time_get_hour())
 													   ,rtc_convert_bcd2bin(rtc_time_get_minute())
 													   ,rtc_convert_bcd2bin(rtc_time_get_second()));
-
-	printf("Time : %.2d : %.2d : %.2d\n\r"             ,rtc_convert_bcd2bin(rtc_time_get_hour())
-													   ,rtc_convert_bcd2bin(rtc_time_get_minute())
-													   ,rtc_convert_bcd2bin(rtc_time_get_second()));
-
-
 }
 
 static void uart_callback(void)
@@ -52,17 +47,29 @@ static void uart_callback(void)
 														   ,rtc_convert_bcd2bin(rtc_date_get_month())
 														   ,rtc_convert_bcd2bin(rtc_date_get_year()));
 
-	if(key == '1')
+	if(key == 'D')
 		{
 		printf("Date : %.2d - %.2d - %.2d\n\r" 			   ,rtc_convert_bcd2bin(rtc_date_get_day())
-															   ,rtc_convert_bcd2bin(rtc_date_get_month())
-															   ,rtc_convert_bcd2bin(rtc_date_get_year()));
+														   ,rtc_convert_bcd2bin(rtc_date_get_month())
+														   ,rtc_convert_bcd2bin(rtc_date_get_year()));
 		}
+	else if(key == 'T')
+	{
+		printf("Time : %.2d : %.2d : %.2d\n\r"             	   ,rtc_convert_bcd2bin(rtc_time_get_hour())
+															   ,rtc_convert_bcd2bin(rtc_time_get_minute())
+															   ,rtc_convert_bcd2bin(rtc_time_get_second()));
+	}
+	else if(key == 'A')
+	{
+		printf("Alarm set for %s\n\r" , alarm_time);
+		alr_set = 1;
+
+	}
 }
 
 void USART2_IRQHandler(void)
 {
-	/*ChecK if RXNE is set*/
+	/*Check if RXNE is set*/
 
 	if(USART2-> ISR & ISR_RXNE)
 	{
@@ -73,13 +80,22 @@ void USART2_IRQHandler(void)
 
 static void SysTick_callback(void)
 {
-	display_calendar();
+	get_calendar();
 	GPIO_set_light();
+	if(alr_set)
+	{
+		if(strcmp((char *)time_buffer , alarm_time) == 0)
+		{
+			printf("ALARM RINGING!!!!!");
+		}
+	}
 }
 
 void SysTick_Handler(void)
 {
 	SysTick_callback();
 }
+
+
 
 
